269e237485e4e49a5441f3d0c49fb9bb96e2e212
Maya Metodieva <maya_2001@abv.bg>
2021-01-10 00:26:54 +0200

    File Opening

--- Calculator-Project/Source.cpp
+++ Calculator-Project/Source.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main() {
 
+
+int main() {
+    ifstream myFile;
+    myFile.open("Test1.txt");
+    string input;
+    if (!myFile)
+    {
+        cout << "NaN" << endl;
+        return -1;
+    }
+    else
+    {
+        getline(myFile, input);
+        myFile.close();
+    }
 
 	return 0;
 }
